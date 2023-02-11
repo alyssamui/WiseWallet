321cb6fbda18711e82b926072d2d9afbc3715b24
Justin Pong <jp.waves51@gmail.com>
2023-02-11 18:06:52 -0500

    clean up expense service and create category service

--- src/api/ExpenseService.ts
+++ src/api/ExpenseService.ts
@@ -3,23 +3,15 @@ import { Expense } from "../types/expense";
 const EXPENSE_ID_PREFIX = "expense_";
 
 class ExpenseService {
-  private onSuccess(data: any) {
-    console.log(`Added Income ${JSON.stringify(data)}`);
+  private onSuccess(log: string) {
+    console.log(log);
   }
 
-  private onError(error: any): void {
+  private onError(error: any) {
     console.log(error);
   }
 
-  // private handleResponse(response: any, onSuccess: () => void, onError: () => void, defaultValue = undefined) {
-  //   let data = defaultValue;
-
-  //   response.then((res) => {
-
-  //   })
-  // }
-
-  // acts as both a post and put
+  // POST/PUT
   async setExpense(id: number, data: Expense) {
     const expenseId = EXPENSE_ID_PREFIX + id;
     const payload = {
@@ -38,7 +30,7 @@ class ExpenseService {
 
     response
       .then((res) => {
-        this.onSuccess(res);
+        this.onSuccess(`Added Expense<${expenseId}>: ${JSON.stringify(res)}`);
       })
       .catch((err) => {
         this.onError(err);
@@ -52,7 +44,7 @@ class ExpenseService {
       chrome.storage.local.get([expenseId], (items) => {
         if (chrome.runtime.lastError) {
           reject(
-            `Failed to retrieve entry for income<${expenseId}>: ${chrome.runtime.lastError}`
+            `Failed to retrieve entry for Expense<${expenseId}>: ${chrome.runtime.lastError}`
           );
         } else {
           resolve(items);
@@ -63,8 +55,9 @@ class ExpenseService {
     let data = undefined;
     response
       .then((res) => {
-        // TODO: improve happy path response
-        console.log(res);
+        this.onSuccess(
+          `Retrieved Expense<${expenseId}>: ${JSON.stringify(res)}`
+        );
         data = res;
       })
       .catch((err) => {
@@ -90,15 +83,17 @@ class ExpenseService {
     const data: any[] = [];
     response
       .then((res) => {
-        // TODO: improve happy path response
         Object.entries(res as object).forEach(([key, value]) => {
           if (key.includes(EXPENSE_ID_PREFIX)) {
             data.push({ [key]: value });
           }
         });
 
-        console.log(res);
-        console.log(data);
+        this.onSuccess(
+          `Retrieved all expenses: ${data
+            .map((expense) => `Expense<${Object.keys(expense as object)[0]}>`)
+            .toString()})}`
+        );
       })
       .catch((err) => {
         this.onError(err);
@@ -114,17 +109,17 @@ class ExpenseService {
       chrome.storage.local.remove([expenseId], () => {
         if (chrome.runtime.lastError) {
           reject(
-            `Failed to delete expense<${expenseId}>: ${chrome.runtime.lastError}`
+            `Failed to delete Expense<${expenseId}>: ${chrome.runtime.lastError}`
           );
         } else {
-          resolve(`Deleted expense<${expenseId}>`);
+          resolve(null);
         }
       });
     });
 
     response
       .then((res) => {
-        console.log(res);
+        this.onSuccess(`Deleted expense<${expenseId}>`);
       })
       .catch((err) => {
         this.onError(err);
@@ -144,14 +139,18 @@ class ExpenseService {
         if (chrome.runtime.lastError) {
           reject(`Failed to delete all expenses: ${chrome.runtime.lastError}`);
         } else {
-          resolve("Successfully deleted all expenses");
+          resolve(null);
         }
       });
     });
 
     response
       .then((res) => {
-        console.log(res);
+        this.onSuccess(
+          `Deleted all expenses: ${expenseIds
+            .map((id) => `Expense<${id}`)
+            .toString()}`
+        );
       })
       .catch((err) => {
         this.onError(err);
